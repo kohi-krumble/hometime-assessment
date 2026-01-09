7b3bb67967343f6557548f2b60731f0f716aba34
kohi-krumble <marana.michaelj@gmail.com>
2026-01-09 22:47:03 +0800

    Fix validates method error for custom validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
   validates :start_date, comparison: { greater_than: -> { Date.current } }
-  validates :end_date_after_start_date
+  validate :end_date_after_start_date
   validates :no_of_nights, numericality: { only_integer: true, greater_than: 0 }
   validates :currency, presence: true
   monetize :payout_amount_cents, with_model_currency: :currency,
