51c542b61bb4376bb858ea8dd6b0c41456d9be8b
kohi-krumble <marana.michaelj@gmail.com>
2026-01-10 00:02:45 +0800

    Fix wrong reservation date column name on validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :guest, class_name: 'User', foreign_key: 'user_id'
 
-  validates :start_date, comparison: { greater_than: -> { Date.current } }
-  validate :end_date_after_start_date
+  validates :start_at, comparison: { greater_than: -> { Date.current } }
+  validate :end_at_after_start_at
   validates :no_of_nights, numericality: { only_integer: true, greater_than: 0 }
   validates :currency, presence: true
   monetize :payout_amount_cents, with_model_currency: :currency,
@@ -20,9 +20,9 @@ class Reservation < ApplicationRecord
 
   private
 
-  def end_date_after_start_date
-    return if end_date > start_date
+  def end_at_after_start_at
+    return if end_at > start_at
 
-    errors.add(:end_date, 'must be after the start date')
+    errors.add(:end_at, 'must be after the start date')
   end
 end
