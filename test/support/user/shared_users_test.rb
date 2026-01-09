44caca2712dd01dc5e514239a33bc141d438e429
kohi-krumble <marana.michaelj@gmail.com>
2026-01-09 23:33:18 +0800

    Created shared user model tests

---
+++ test/support/user/shared_users_test.rb
@@ -0,0 +1,32 @@
+module SharedUsersTest
+  extend ActiveSupport::Concern
+
+  included do
+    test "Creates user with valid email" do
+      user = create(:user)
+      assert user.persisted?
+      assert_match Rails.application.config.email_regex, user.email
+    end
+
+    test "Fails to create user with no email" do
+      user = build(:user, :no_email)
+      assert_not user.valid?
+      assert_includes user.errors[:email], "can't be blank"
+    end
+
+    test "Fails to create user with invalid email" do
+      user = build(:user, :invalid_email)
+      assert_not user.valid?
+      assert_includes user.errors[:email], "is invalid"
+    end
+
+    test "Fails to create user with duplicate email" do
+      user1 = create(:user)
+      user2 = build(:user, email: user1.email)
+
+      assert user1.persisted?
+      assert_not user2.valid?
+      assert_includes user2.errors[:email], "has already been taken"
+    end
+  end
+end
