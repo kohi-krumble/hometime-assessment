5a027437c446528d0267f4edeb870519973ff6bb
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 13:19:04 +0800

    Created migration to add user type to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_09_141941) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_11_051319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_09_141941) do
     t.jsonb "phone_numbers"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_type", default: "guest", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["user_type"], name: "index_users_on_user_type"
   end
 
   add_foreign_key "reservations", "users"
