0789c094a99a192df20f98b0b2e2d831469b0daa
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 21:08:56 +0800

    Add an option to disable force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  force_ssl = ENV['FORCE_SSL'].presence || true
+  config.force_ssl = force_ssl == true
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
