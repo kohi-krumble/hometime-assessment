6e046b592e8c48076b13a407e90def09e3fc39ae
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 13:21:50 +0800

    Set default scope and attributte value of guest model to "guest"

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,7 @@
 class Guest < User
+  default_scope { where(user_type: 'guest') }
+
   has_many :reservations, foreign_key: 'user_id', class_name: 'Reservation'
+
+  attribute :user_type, :string, default: 'guest'
 end
