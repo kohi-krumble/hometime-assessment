6d4cb13da9b363583c8979f8a28ecc86f7d2c0b9
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 15:48:53 +0800

    Parse reservertion params based on payload structure

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,6 +1,16 @@
-class Api::ReservationsController < ApplicationController
+class Api::ReservationsController < AuthenticatedController
+  wrap_parameters false
 
   def create
-    render json: { message: "Reservation created successfully" }, status: :created
+    reservation_params = Reservation::PayloadParser.new(parser.new(params)).parse
+
+    render json: reservation_params, status: :created
+  end
+
+  private
+
+  def parser
+    return Reservation::PayloadV2 if params.key?(:reservation)
+    return Reservation::PayloadV1
   end
 end
