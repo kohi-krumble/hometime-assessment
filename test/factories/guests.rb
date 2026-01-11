dedda644d85409c11ddc78bba0cbc1e4a1e9b6ba
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 13:22:11 +0800

    Added test to guest test to check user type

--- test/factories/guests.rb
+++ test/factories/guests.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :guest, parent: :user do
     email { "guest@example.com" }
     first_name { "Guest user" }
+    user_type { 'guest' }
   end
 end
--- test/models/guest_test.rb
+++ test/models/guest_test.rb
@@ -3,6 +3,13 @@ require "test_helper"
 class GuestTest < ActiveSupport::TestCase
   include SharedUsersTest
 
+  test "User type should be guest" do
+    guest = create(:guest)
+
+    assert guest.persisted?
+    assert guest.guest?
+  end
+
   test "Guest model should have reservations association" do
     associations = Guest.reflect_on_all_associations.map(&:name)
 
