b556764ad55d3373298fe8bee9d4860d5cc5b924
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 19:59:30 +0800

    Added authentication on requests

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,9 +1,18 @@
 class AuthenticatedController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_request!
 
   private
 
-  def authenticate_user!
-    true
+  def authenticate_request!
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header.present?
+    return unauthorized! if token.blank?
+
+    decoded = JwtService.decode(token)
+    return unauthorized! if decoded.blank?
+  end
+
+  def unauthorized!
+    render json: { error: "Unauthorized" }, status: :unauthorized
   end
 end
