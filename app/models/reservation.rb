4b0a8eacff398855d756b0f00f621c5e45039801
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 13:12:00 +0800

    Fix spacing

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,11 +5,11 @@ class Reservation < ApplicationRecord
   validates :no_of_nights, numericality: { only_integer: true, greater_than: 0 }
   validates :currency, presence: true
   monetize :payout_amount_cents, with_model_currency: :currency,
-  numericality: { greater_than_or_equal_to: 0 }
+           numericality: { greater_than_or_equal_to: 0 }
   monetize :security_amount_cents, with_model_currency: :currency,
-  numericality: { greater_than_or_equal_to: 0 }
+           numericality: { greater_than_or_equal_to: 0 }
   monetize :total_amount_cents, with_model_currency: :currency,
-  numericality: { greater_than_or_equal_to: 0 }
+           numericality: { greater_than_or_equal_to: 0 }
   validates :details, reservation_details: true
   validates :end_at, reservation_end_at: true
 
