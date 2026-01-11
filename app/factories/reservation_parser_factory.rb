4707af18ee8554f199e847f5cc8c47e89f45d9af
kohi-krumble <marana.michaelj@gmail.com>
2026-01-11 18:25:57 +0800

    Created separate factory for resevation payload parser

---
+++ app/factories/reservation_parser_factory.rb
@@ -0,0 +1,30 @@
+class ReservationParserFactory
+
+  def initialize(payload)
+    @payload = payload
+  end
+
+  def get_parser(version: nil)
+    return parser_by_version(version) if version.present?
+    return parser_v2 if @payload.key?(:reservation)
+    return parser_v1
+  end
+
+  private
+
+  def parser_by_version(version)
+    case version
+    when :v1 then parser_v1
+    when :v2 then parser_v2
+    else raise "Unsupported parser version: #{version}"
+    end
+  end
+
+  def parser_v1
+    @parser_v1 ||= Reservation::PayloadV1.new(@payload)
+  end
+
+  def parser_v2
+    @parser_v2 ||= Reservation::PayloadV2.new(@payload)
+  end
+end
